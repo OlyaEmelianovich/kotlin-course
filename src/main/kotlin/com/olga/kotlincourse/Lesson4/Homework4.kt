ad7c7e91c548f618c15416dd195790831ae90219
Volha, Yemelyanovich <volha.emelyanovich@gmail.com>
2024-09-29 13:46:44 +0300

    homework 3

---
+++ src/main/kotlin/com/olga/kotlincourse/Lesson4/Homework4.kt
@@ -0,0 +1,44 @@
+package com.olga.com.olga.kotlincourse.Lesson4
+
+class {Для каждого значения допиши подходящий тип данных
+}
+
+val Int_1: Int = 42
+val Long_1: Long = 98765432123456789L
+val Float_1: Float = 23.45f
+val Double_1: Double = 0.123456789
+val String_1: String = "Kotlin & Java"
+val Boolean_1: Boolean = false
+val Char: Char = 'c'
+val Int_2: Int = 500
+val Long_2: Long = 4294967296L
+val Float_2: Float = 18.0F
+val Double_2: Double = -0.001
+val String_2: String = "OpenAI"
+val String_3: String = "true"
+val Char_1: Char ='9'
+val Int_3: Int = 2048
+val Long_3: Long = 10000000000L
+val Float_3: Float = 5.75f
+val Double_3: Double = 1.414
+val String_4: String = "Artificial Intelligence"
+val Boolean_3: Boolean = false
+val Char_: Char = '@'
+val Int_4: Int = 1024
+val Long_4: Long = 1234567890123L
+val Float_4: Float = 10.01f
+val Double_4: Double = -273.15
+val String_5: String = "SpaceX"
+val Boolean_4: Boolean = true
+val String_6: String = "🤯"
+val Char_2: Int = 65535
+val Long_5: Long = 72057594037927935L
+val Float_5: Float = 2.71828f
+val Double_5: Double = 101.0101
+val String_7: String = "Quantum Computing"
+val Boolean_5: Boolean = false
+val Char_3: Char = 'X'
+val Int_5: Int = 314
+val Long_6: Long = 123456789123456789L
+val Float_6: Float = 6.626F
+val Double_6: Double = 0.007
